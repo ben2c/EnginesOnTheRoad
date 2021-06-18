436724f9ded9fbc13ae382a389cba4e71542b972
Ben <bencheng200@gamil.com>
2021-06-18 17:50:19 -0500

    implenting fetch for car data

---
+++ engines-frontend/src/index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const endPoint = 'http://localhost:3000/cars';
+  fetch(endPoint)
+    .then(res => res.json())
+    .then(json =>
+      json.forEach(car => {
+        const markup = `
+        <li>
+          <h3>${car.make}
+            <button>Edit</button>
+          </h3>
+        </li>`;
+
+        document.querySelector('#car-list').innerHTML += markup;
+      })
+    );
+});
