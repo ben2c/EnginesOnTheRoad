b42b0694d2cbfa664560e823c49a0b16f2bc46cf
Ben <bencheng200@gamil.com>
2021-06-19 00:36:33 -0500

    added delete and submit method to car

--- engines-frontend/src/car.js
+++ engines-frontend/src/car.js
@@ -32,9 +32,42 @@ class Car {
         }
 
       })
-
   }
   
+  static submitCar(car) {
+    car.preventDefault()
+    fetch(carsURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify({
+        make: makeInput.value,
+        model: modelInput.value,
+        year: yearInput.value,
+        trim: trimInput.value,
+        image: imageInput.value
+      })
+
+    })
+      .then(response => response.json())
+      .then(car => {
+        //let newCar = new Car(car.data)
+        carForm.reset()
+      })
+  }
+
+  deleteCar() {
+
+    const carId = this.parentElement.dataset.id
+
+    fetch(`${carsURL}/${carId}`, {
+      method: "DELETE"
+    })
+
+    this.parentElement.remove()
+  }
 
 }
 
--- engines-frontend/src/index.js
+++ engines-frontend/src/index.js
@@ -1,4 +1,12 @@
 const carsURL = 'http://localhost:3000/cars'
+const reviewsURL = 'http://localhost:3000/reviews'
+const carForm = document.getElementById("car-form")
+const makeInput = document.getElementById("input-make")
+const modelInput = document.getElementById("input-model")
+const yearInput = document.getElementById("input-year")
+const trimInput = document.getElementById("input-trim")
+const imageInput = document.getElementById("input-image_url")
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const endPoint = 'http://localhost:3000/cars';
