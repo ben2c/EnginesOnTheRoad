fd061f2f64cd7b4db56b121ee81f0a034c78f36e
Ben <bencheng200@gamil.com>
2021-06-19 12:28:01 -0500

    updated render car for frontend

--- engines-frontend/src/car.js
+++ engines-frontend/src/car.js
@@ -19,7 +19,6 @@ class Car {
     carList.innerHTML = ""
     for (let car of cars) {
       car.renderCar()
-
     }
   }
 
@@ -30,7 +29,6 @@ class Car {
         for (let car of cars.data) {
           let newCarList = new Car(car)
         }
-
       })
   }
   
@@ -49,7 +47,6 @@ class Car {
         trim: trimInput.value,
         image: imageInput.value
       })
-
     })
       .then(response => response.json())
       .then(car => {
@@ -65,9 +62,51 @@ class Car {
     fetch(`${carsURL}/${carId}`, {
       method: "DELETE"
     })
-
     this.parentElement.remove()
   }
 
+  renderCar() {
+
+    const carLi = document.createElement('li')
+
+    carLi.dataset.id = this.id
+    carList.appendChild(carLi)
+    const h3 = document.createElement('h3')
+    h3.className = ("card-header")
+    h3.innerText = this.name
+    const img = document.createElement('img')
+    img.src = this.image
+    img.width = 200
+    const p = document.createElement('p')
+    p.className = "card-text"
+    p.innerText = this.instructions
+
+    //delete button
+    const deleteBtn = document.createElement("button")
+    deleteBtn.className = "btn btn-primary btn-sm"
+    deleteBtn.innerText = "Remove Car"
+    deleteBtn.addEventListener("click", this.deleteCar)
+
+    //review form
+    const reviewForm = document.createElement('form')
+    reviewForm.innerHTML += `<input type="text"  class="form-control" id="review-input" placeholder ="Review">
+        <input type="submit" class="btn btn-primary btn-sm" value="Add">`
+
+
+    reviewForm.addEventListener("submit", Review.createReview)
+
+    const reviewList = document.createElement("ul")
+    reviewList.className = "list-group list-group-flush"
+    reviewList.dataset.id = this.id
+    //rendering each review for a car
+
+    this.reviews.forEach(review => {
+
+      let newReview = new Review(review)
+
+      newReview.renderReview(reviewList)
+    })
+    reviewLi.append(h3, img, reviewList, reviewForm, p, deleteBtn)
+  }
 }
 
--- engines-frontend/src/index.js
+++ engines-frontend/src/index.js
@@ -1,27 +1,23 @@
 const carsURL = 'http://localhost:3000/cars'
 const reviewsURL = 'http://localhost:3000/reviews'
 const carForm = document.getElementById("car-form")
+const part = document.getElementById("part")
 const makeInput = document.getElementById("input-make")
 const modelInput = document.getElementById("input-model")
 const yearInput = document.getElementById("input-year")
 const trimInput = document.getElementById("input-trim")
 const imageInput = document.getElementById("input-image_url")
+const carList = document.getElementById("car-list")
 
 
-document.addEventListener('DOMContentLoaded', () => {
-  const endPoint = 'http://localhost:3000/cars';
-  fetch(endPoint)
-    .then(res => res.json())
-    .then(json =>
-      json.forEach(car => {
-        const markup = `
-        <li>
-          <h3>${car.make}
-            <button>Edit</button>
-          </h3>
-        </li>`;
+part.addEventListener("DOMContentLoaded", function (x) {
+  let arr = Car.allCars.filter(car => {
+    for (let view of car.reviews) {
+      if (view.make.toLowerCase().includes(x.target.value)) {
+        return true
+      }
+    }
+  })
 
-        document.querySelector('#car-list').innerHTML += markup;
-      })
-    );
-});
+  Car.renderCars(arr)
+})
